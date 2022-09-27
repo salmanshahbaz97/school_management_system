38127e9397bd16a6cb2f21c8e06427a97b771ff6
Salman Shahbaz <salman.shahbaz@apl.com.pk>
2022-09-28 00:59:49 +0500

    Set Prod and Dev files

---
+++ school_management/settings/dev.py
@@ -0,0 +1,26 @@
+from .common import *
+
+
+DEBUG = True
+SECRET_KEY = 'django-insecure-2ghmr%ud^drtax^pa&&sa(y)!msrcn_6q^c4m&@-wgrbf33!2c'
+
+# for mysql database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_management_db',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'salman.123'
+    }
+}
+# for postgres database
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'school_management_system_db',
+#         'USER': 'postgres',
+#         'PASSWORD': '11223344',
+#         'PORT': 5433
+#     }
+# }
