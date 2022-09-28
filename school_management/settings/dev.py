43184244c72c2112b29fe9572cde1f0f5956b07b
Salman Shahbaz <87059318+salmanshahbaz97@users.noreply.github.com>
2022-09-29 01:47:02 +0500

    Update dev.py

--- school_management/settings/dev.py
+++ school_management/settings/dev.py
@@ -5,22 +5,22 @@ DEBUG = True
 SECRET_KEY = 'django-insecure-2ghmr%ud^drtax^pa&&sa(y)!msrcn_6q^c4m&@-wgrbf33!2c'
 
 # for mysql database
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'school_management_db',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': 'salman.123'
-    }
-}
-# for postgres database
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'school_management_system_db',
-#         'USER': 'postgres',
-#         'PASSWORD': '11223344',
-#         'PORT': 5433
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'school_management_db',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': 'salman.123'
 #     }
 # }
+# for postgres database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'school_management_system_db',
+        'USER': 'postgres',
+        'PASSWORD': '11223344',
+        'PORT': 5433
+    }
+}
